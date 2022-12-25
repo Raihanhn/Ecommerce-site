e61de4407f978a600112ff965150981d194b5798
raihanhn <raihanhn20@gmail.com>
2022-12-26 03:12:34 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Navbar from "./components/Navbar/Navbar"
+
+function App() {
+
+  return (
+    <div>
+      <Navbar />
+      <div className="container">
+        <article>
+          <h1>What is Lorem Ipsum? </h1>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry...
+        </article>
+      </div>
+    </div>
+  )
+}
+
+export default App

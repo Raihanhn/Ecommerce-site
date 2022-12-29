8304756134a6c1279b65fe7a03e02ca9e799e5b9
raihanhn <raihanhn20@gmail.com>
2022-12-29 22:45:07 +0600

    2nd commit

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import { useState } from 'react'
+import products from './Products'
 
 const Product = () => {
+
+  const [data, setData] = useState(products);
+
   return (
     <div className='akeel'>
-         <section className="about" id="about">
+         <section className="product">
             <h1 className="heading"> our <span>products</span> </h1>
             <div className="box-container">
+            {data.map((value) =>{
+                const {id, title, desc, bttn, image} = value;
+
+                return (
+                    <>
+                        <div className="box" key={id}>
+                          <img src={image} alt="" />
+                         <h3> {title} </h3>
+                          <p>{desc}</p>
+                          <a href="#" className='mybtns'> {bttn} </a>
+                        </div>
+                    </>
+                )
+            })}
             </div>
         </section>
     </div>
---
+++ src/components/Products.js
@@ -0,0 +1,232 @@
+import img1 from "../assets/productos/m1.jpg"
+import img2 from "../assets/productos/m2.jpg"
+import img3 from "../assets/productos/m3.jpg"
+import img4 from "../assets/productos/m4.jpg"
+import img5 from "../assets/productos/p1.jpg"
+import img6 from "../assets/productos/p2.jpg"
+import img7 from "../assets/productos/p3.jpg"
+import img8 from "../assets/productos/p4.jpg"
+import img9 from "../assets/productos/w1.jpg"
+import img10 from "../assets/productos/w2.jpg"
+import img11 from "../assets/productos/w3.jpg"
+import img12 from "../assets/productos/w4.jpg"
+import img13 from "../assets/productos/d1.jpg"
+import img14 from "../assets/productos/d2.jpg"
+import img15 from "../assets/productos/d3.jpg"
+import img16 from "../assets/productos/d4.jpg"
+import img17 from "../assets/productos/h1.jpg"
+import img18 from "../assets/productos/h2.jpg"
+import img19 from "../assets/productos/h3.jpg"
+import img20 from "../assets/productos/h4.jpg"
+import img21 from "../assets/productos/b1.jpg"
+import img22 from "../assets/productos/b2.jpg"
+import img23 from "../assets/productos/b3.jpg"
+import img24 from "../assets/productos/b4.jpg"
+import img25 from "../assets/productos/t1.jpg"
+import img26 from "../assets/productos/t2.jpg"
+import img27 from "../assets/productos/t3.jpg"
+import img28 from "../assets/productos/t4.jpg"
+
+
+
+const Products = [
+    {
+        id:1,
+        title:"man shirt",
+        desc:"upto 30% off",
+        bttn :"shop now",
+        image:img1
+    },
+    {
+        id:2,
+        title:"man shirt",
+        desc:"upto 30% off",
+        bttn :"shop now",
+        image:img2
+    },
+    {
+        id:3,
+        title:"man shirt",
+        desc:"upto 30% off",
+        bttn :"shop now",
+        image:img3
+    },
+    {
+        id:4,
+        title:"man shirt",
+        desc:"upto 30% off",
+        bttn :"shop now",
+        image:img4
+    },
+    {
+        id:5,
+        title:"man phant",
+        desc:"upto 30% off",
+        bttn :"shop now",
+        image:img5
+    },
+    {
+        id:6,
+        title:"man phant",
+        desc:"upto 30% off",
+        bttn :"shop now",
+        image:img6
+    },
+    {
+        id:7,
+        title:"man phant",
+        desc:"upto 30% off",
+        bttn :"shop now",
+        image:img7
+    },
+    {
+        id:8,
+        title:"man phant",
+        desc:"upto 30% off",
+        bttn :"shop now",
+        image:img8
+    },
+    {
+        id:9,
+        title:"woman dress",
+        desc:"upto 30% off",
+        bttn :"shop now",
+        image:img9
+    },
+    {
+        id:10,
+        title:"woman dress",
+        desc:"upto 30% off",
+        bttn :"shop now",
+        image:img10
+    },
+    {
+        id:11,
+        title:"woman dress",
+        desc:"upto 30% off",
+        bttn :"shop now",
+        image:img11
+    },
+    {
+        id:12,
+        title:"woman dress",
+        desc:"upto 30% off",
+        bttn :"shop now",
+        image:img12
+    },
+    {
+        id:13,
+        title:"electronic product",
+        desc:"upto 30% off",
+        bttn :"shop now",
+        image:img13
+    },
+    {
+        id:14,
+        title:"electronic product",
+        desc:"upto 30% off",
+        bttn :"shop now",
+        image:img14
+    },
+    {
+        id:15,
+        title:"electronic product",
+        desc:"upto 30% off",
+        bttn :"shop now",
+        image:img15
+    },
+    {
+        id:16,
+        title:"electronic product",
+        desc:"upto 30% off",
+        bttn :"shop now",
+        image:img16
+    },
+    {
+        id:17,
+        title:"cooking product",
+        desc:"upto 30% off",
+        bttn :"shop now",
+        image:img17
+    },
+    {
+        id:18,
+        title:"cooking product",
+        desc:"upto 30% off",
+        bttn :"shop now",
+        image:img18
+    },
+    {
+        id:19,
+        title:"cooking product",
+        desc:"upto 30% off",
+        bttn :"shop now",
+        image:img19
+    },
+    {
+        id:20,
+        title:"cooking product",
+        desc:"upto 30% off",
+        bttn :"shop now",
+        image:img20
+    },
+    {
+        id:21,
+        title:"baby dress",
+        desc:"upto 30% off",
+        bttn :"shop now",
+        image:img21
+    },
+    {
+        id:22,
+        title:"baby dress",
+        desc:"upto 30% off",
+        bttn :"shop now",
+        image:img22
+    },
+    {
+        id:23,
+        title:"baby dress",
+        desc:"upto 30% off",
+        bttn :"shop now",
+        image:img23
+    },
+    {
+        id:24,
+        title:"baby dress",
+        desc:"upto 30% off",
+        bttn :"shop now",
+        image:img24
+    },
+    {
+        id:25,
+        title:"baby toy",
+        desc:"upto 30% off",
+        bttn :"shop now",
+        image:img25
+    },
+    {
+        id:26,
+        title:"baby toy",
+        desc:"upto 30% off",
+        bttn :"shop now",
+        image:img26
+    },
+    {
+        id:27,
+        title:"baby toy",
+        desc:"upto 30% off",
+        bttn :"shop now",
+        image:img27
+    },
+    {
+        id:28,
+        title:"baby toy",
+        desc:"upto 30% off",
+        bttn :"shop now",
+        image:img28
+    }
+   
+ ]
+
+ export default Products;

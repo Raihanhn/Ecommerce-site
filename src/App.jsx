f6c6cfdbffee6ce35be7b7b6a369c1851e492dca
raihanhn <raihanhn20@gmail.com>
2022-12-31 18:49:07 +0600

    6th commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Contact from './components/Contact'
+import Footer from './components/Footer'
 import Home from './components/Home'
 import Navbar from "./components/Navbar/Navbar"
 import Product from './components/Product'
@@ -14,6 +15,7 @@ function App() {
       <Product/>
       <Review/>
       <Contact/>
+      <Footer/>
     </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { AiFillFacebook } from 'react-icons/ai';
+import { AiFillTwitterSquare } from 'react-icons/ai';
+import { AiFillInstagram } from 'react-icons/ai';
+import { AiFillLinkedin } from 'react-icons/ai';
+import { BsFillTelephoneFill } from 'react-icons/bs';
+import { GrMail } from 'react-icons/gr';
+import { FaMapMarkerAlt } from 'react-icons/fa';
+
+const Footer = () => {
+  return (
+    <section className="footer">
+      <div className="box-container">
+
+        <div className="box">
+            <h3> Eshop </h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste, quos.</p>
+            <div className="share">
+                <a href="#"> <AiFillFacebook/> </a>
+                <a href="#"> <AiFillTwitterSquare /> </a>
+                <a href="#"> <AiFillInstagram /> </a>
+                <a href="#"> <AiFillLinkedin /> </a>
+            </div>
+            <p>all right reserved ! <br/> <strong>created by mohammad Raihan</strong> </p>
+        </div> 
+        
+       <div class="box">
+        <h3> contact info</h3>
+        <a href="#" className="links"> <BsFillTelephoneFill/> +123 -456-7890</a>
+        <a href="#" className="links"> <BsFillTelephoneFill/> +111 -112-7840</a>
+        <a href="#" className="links"> <GrMail/> Raihanhn@gmail.com</a>
+        <a href="#" className="links"> <FaMapMarkerAlt/> chittagong,Bangladesh - 4203</a>
+        </div> 
+
+        <div className="box">
+           <h3> newsletter</h3>
+           <p>subscribe for latest updates</p>
+           <input type="email" placeholder="your email" className="email"/>
+           <input type="submit" value="suscribe" className="mybtn"/>
+          
+          </div> 
+
+        
+
+
+
+
+
+
+
+      </div>
+    </section>
+  )
+}
+
+export default Footer
